5746f5e2808c1185187005fb085b8c6cba738c15
rainbowmole <camachonathaniel40@gmail.com>
2025-09-15 23:36:22 +0800

    updated version of app dev

--- lib/pages/act1_page.dart
+++ lib/pages/act1_page.dart
@@ -125,12 +125,9 @@ class _YourMusicPlayer extends State<ActivityPage1>{
 
             Image.asset(
               cover[_currentIndex % cover.length],
-              width: 200,
-              height: 200,
+              width: 350,
+              height: 350,
               fit: BoxFit.cover,
-              errorBuilder: (context, error, stackTrace) {
-                return const Icon(Icons.accessibility, size: 200, color: Colors.blueGrey);
-              },
             ),
             
             const SizedBox(height: 40),
